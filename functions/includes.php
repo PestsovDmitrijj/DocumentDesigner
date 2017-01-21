ae10df7495a99802e998d28abbc0cd13bf2b2a18
PestsovDmitrijj <pestsovdmitrijj@gmail.com>
2017-01-21 16:42:38 +0300

    Create a folder for functions

---
+++ functions/includes.php
@@ -0,0 +1,9 @@
+<?php
+// ==================================================
+// This file collects all the files of this directory
+// ==================================================
+
+//to verify that the connection file
+include 'testInclude.php';
+
+?>
--- getData.php
+++ getData.php
@@ -1,4 +1,5 @@
 <?php
+include './functions/includes.php';
 //ini_set('display_errors',1);
 //error_reporting(E_ALL);
 
